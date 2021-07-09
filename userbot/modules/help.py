14f12c208af3ae84af8ea48f321c1daeed1b7f93
Daffansaa <85962930+Daffansaa@users.noreply.github.com>
2021-07-09 15:13:57 +0700

    ler

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -32,7 +32,7 @@ async def help(event):
         string = ""
         for i in CMD_HELP:
             string += "`" + str(i)
-            string += "`\t 📚  "
+            string += "`\t 🐣  "
         await event.edit("** 🐣 CAN-UBOT 🐣 **\n\n"
                          f"**◉ Bᴏᴛ ᴏꜰ {DEFAULTUSER}**\n**◉ Mᴏᴅᴜʟᴇꜱ : {len(modules)}**\n\n"
                          "**• Mᴀɪɴ Mᴇɴᴜ :**\n"

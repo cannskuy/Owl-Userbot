23a9f823622df72b367b06da49aa669953cd110f
canskuy <71593098+maspion27@users.noreply.github.com>
2021-07-09 19:28:10 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f" 🐣 CAN-UBOT 🐣  V{BOT_VER} [MENYALA BRADS!]")
+    f" 🦉 Owl-Userbot 🦉  V{BOT_VER} [MENYALA BRADS!]")
 
 
 if len(argv) not in (1, 3, 4):

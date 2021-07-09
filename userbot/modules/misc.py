50b4c26dce2375a43ede03d7db61d3e8fc90cfad
canskuy <71593098+maspion27@users.noreply.github.com>
2021-07-09 20:41:41 +0700

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -110,12 +110,12 @@ async def repeat(rep):
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
-        "~~~~~~~~~~~~~~~~\n"
-        "│    𝐑𝐄𝐏𝐎  : [🦉Owl-Userbot🦉](https://github.com/maspion27/Owl-Userbot)\n"
-        "~~~~~~~~~~~~~~~~\n"
-        "│🔥 **𝐎𝐰𝐧𝐞𝐫 :** [Can](t.me/inicannn)\n"
-        "~~~~~~~~~~~~~~~~\n"
-
+        "--------------\n"
+        "│    ʀᴇᴘᴏ   : [🦉Owl-Userbot🦉](https://github.com/maspion27/Owl-Userbot)\n"
+        "├-------------\n"
+        "│🔥 **ᴏᴡɴᴇʀ :** [Can](t.me/inicannn)\n"
+        "--------------\n"
+      
     )
 
 

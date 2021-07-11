095bd2a95e8c62bdccf41f8df067b70c0c08d2b1
canskuy <71593098+maspion27@users.noreply.github.com>
2021-07-11 13:54:40 +0700

    owl update

--- userbot/modules/bitly.py
+++ userbot/modules/bitly.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2020 azrim.
 # All rights reserved.
-#
+# owl
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 #

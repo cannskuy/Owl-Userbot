8aaf862753b6eba20bb727465cb15ba2f21f24bd
canskuy <71593098+maspion27@users.noreply.github.com>
2021-07-11 14:41:25 +0700

    owl update

--- userbot/modules/tags.py
+++ userbot/modules/tags.py
@@ -1,5 +1,5 @@
-# Port By @VckyouuBitch From Geez - Project
-# Copyright © Geez - Project
+# Port By @inicannn From About - userbot
+# Copyright © About - userbot
 # Credits By Ultroid
 
 from telethon.tl.types import ChannelParticipantAdmin as admin

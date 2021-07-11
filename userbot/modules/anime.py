f6795b67cda3d95aaa841191fcd51d5783ce80d5
canskuy <71593098+maspion27@users.noreply.github.com>
2021-07-11 13:52:49 +0700

    owl update

--- userbot/modules/anime.py
+++ userbot/modules/anime.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2020 The Raphielscape Company LLC.
-#
+# owl
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 #

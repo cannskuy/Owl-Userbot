403c5a11403ea1b63515d8ee0daaf11e62b7ef78
canskuy <71593098+maspion27@users.noreply.github.com>
2021-07-11 14:32:42 +0700

    owl update

--- userbot/modules/quotly.py
+++ userbot/modules/quotly.py
@@ -3,7 +3,7 @@
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 #
-# Port From UniBorg to Geez-Project To Vckyouuu
+# Port From UniBorg to About-userbot To cann
 
 
 import random
